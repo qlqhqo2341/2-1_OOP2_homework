bed529b1d0fb44114bfc0437d9deac49346ee546
jeonghan <jeonghan@jeonghan-pc>
2015-05-16 01:31:05 +0900

    간단한 버튼예제 생성

---
+++ CppClassViewer/src/showMe.java
@@ -0,0 +1,41 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+public class showMe extends JFrame{
+	JButton button;
+	String a="æ»≥Á«œΩ≈∞°.", b="»Â¬S.";
+	
+	public showMe(){
+		
+		
+		setSize(300,300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		JPanel panel = new JPanel();
+		button = new JButton(a);
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				if(e.getSource() == button){
+					if(button.getText() == a)
+						button.setText(b);
+					else
+						button.setText(a);
+				}
+			}
+		}
+		);
+		
+		panel.add(button);
+		add(panel);
+		
+		
+		setVisible(true);		
+	}
+	
+}
