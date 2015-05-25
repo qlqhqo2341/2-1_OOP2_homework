ac41a0189a5191222c73d16e69f503b30545caf2
jeonghan <jeonghan@jh-acerbook>
2015-05-25 12:38:06 +0900

    making_parser

--- CppClassViewer/src/working/Parser.java
+++ CppClassViewer/src/working/Parser.java
@@ -1,7 +1,5 @@
 package working;
 
-
-
 import java.util.StringTokenizer;
 import java.util.Vector;
 
@@ -9,22 +7,57 @@ import struct.*;
 public class Parser {
 	CppClass obj;
 	String allText;
+	StringTokenizer mainToken;
 	
 	
 	
 	
 	public Parser(String text){
 		allText=text;
-
-		String range[] = divide();
+		mainToken = new StringTokenizer(allText, "{");
+		
 	}
 
-	public String[] divide(){
-		StringTokenizer token = new StringTokenizer(allText, "{");
-		StringBuffer buffer;
-		Vector<String> range;
+	public String[] nextFunction(){
+
+		String part[] = new String[2];
+		String define;
+		StringBuffer body = new StringBuffer();
+		
+		int closer=0;
+		int last_closer=-1;
+		
+		if(mainToken.hasMoreTokens()){
+			define=mainToken.nextToken();
+		}
+		else
+			return null;
 		
 		
+		while(mainToken.hasMoreTokens() && closer<=0){
+			closer++;
+			StringBuffer t = new StringBuffer();
+			
+			t.append(mainToken.nextToken());
+			for(int k=0;k<t.length();k++){
+				if(t.charAt(k)=='}'){
+					closer--;
+					last_closer=k+body.length();
+				}
+			}
+			body.append(t);
+		}
+		
+		if(closer>0)
+			return null;
+		else{
+			if(last_closer >= 0)
+				body.delete(last_closer, body.length());
+			
+			part[0]=define;
+			part[1]=body.toString();
+			return part;
+		}
 		
 	}
 
